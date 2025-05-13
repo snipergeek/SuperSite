cb2ebb7cf598d0c1683538cda5d336dd6ded6e22
snipergeek <167969584+snipergeek@users.noreply.github.com>
2025-05-13 22:26:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ window.addEventListener('load', function() {
 
 const sitesPrincipaux = [
   { nom: "Animés", url: "https://anime-sama.fr/", type: "anime" },
-  { nom: "Films & Séries", url: "https://vvw.french-stream.bio/", type: "films" },
+  { nom: "Films & Séries", url: "https://fsmirror57.lol/", type: "films" },
   { nom: "TV", url: "https://www.france.tv/", type: "autre" },
   { nom: "Jeux", url: "https://a6023.wordpress.com/", type: "autre" }
 ];
