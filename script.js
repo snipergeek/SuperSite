79470ffe618b20701c72a4bd4c0df8933355bdce
snipergeek <167969584+snipergeek@users.noreply.github.com>
2025-10-06 21:23:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ window.addEventListener('load', function() {
 
 const sitesPrincipaux = [
   { nom: "Animés", url: "https://anime-sama.fr/", type: "anime" },
-  { nom: "Films & Séries", url: "https://moviesvn.net/?riskified_session_id=riskified_LuZD8CDMhgtHnVQq", type: "films" },
+  { nom: "Films & Séries", url: "https://moviesvn.net/", type: "films" },
   { nom: "TV", url: "https://fstv.fun/", type: "autre" },
   { nom: "Jeux", url: "https://a6023.wordpress.com/", type: "autre" }
 ];
