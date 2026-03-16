93a4a78ba64468d5fd4dc163f221ac9281c062d4
snipergeek <167969584+snipergeek@users.noreply.github.com>
2026-03-16 20:34:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ window.addEventListener('load', function() {
 
 const sitesPrincipaux = [
   { nom: "Animés", url: "https://anime-sama.org/", type: "anime" },
-  { nom: "Films & Séries", url: "https://movix.blog/", type: "films" },
+  { nom: "Films & Séries", url: "https://movix.rodeo", type: "films" },
   { nom: "TV", url: "https://fstv.fun/", type: "autre" },
   { nom: "Jeux", url: "https://a6023.wordpress.com/", type: "autre" }
 ];
